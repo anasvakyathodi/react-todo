5119f0446af900d3e37755bd1032997bf48ab83f
anasvakyathodi <anasvakyathodi@gmail.com>
2020-05-15 07:36:29 +0530

    Updates

---
+++ precache-manifest.aa3488160779144cc2bceb2b1db40ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b792dbd987ca65f16920e5faf8f37d7",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "606b8292c5adf7a375ed",
+    "url": "/todo-react/static/css/main.9234a5aa.chunk.css"
+  },
+  {
+    "revision": "003ea4b53b2555cba599",
+    "url": "/todo-react/static/js/2.e236ddcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.e236ddcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606b8292c5adf7a375ed",
+    "url": "/todo-react/static/js/main.cb11a45c.chunk.js"
+  },
+  {
+    "revision": "39d51d9bc1349eb056a9",
+    "url": "/todo-react/static/js/runtime-main.b10d5a97.js"
+  }
+]);
